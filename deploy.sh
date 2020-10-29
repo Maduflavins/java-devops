6ce28c188673d6dbb1089a2066de875deb91b1ea
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-10-29 14:56:20 +0100

    corrected some files

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 docker ps
 docker container ls
 sudo docker login --username $HEROKU_DOCKER_USERNAME --password $HEROKU_AUTH_TOKEN registry.heroku.com
-sudo docker tag reeach-backend:latest registry.heroku.com/devops/web
-if [ BRANCH_NAME == "master" ] && [ PULL_REQUEST == "false" ]; then sudo docker push registry.heroku.com/devops/web; fi
+sudo docker tag java-devops:latest registry.heroku.com/devops/web
+if [ BRANCH_NAME == "master" ] && [ PULL_REQUEST == "false" ]; then sudo docker push registry.heroku.com/java-devops/web; fi
 
 chmod +x heroku-container-release.sh
 sudo chown $USER:docker ~/.docker
